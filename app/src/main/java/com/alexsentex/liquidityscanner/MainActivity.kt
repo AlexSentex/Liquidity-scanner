e760d633526ce1154feedff520149eec088f3b6a
Oleksandr <107505831+AlexSentex@users.noreply.github.com>
2026-09-18 03:39:15 +0300

    Add files via upload

--- app/src/main/java/com/alexsentex/liquidityscanner/MainActivity.kt
+++ app/src/main/java/com/alexsentex/liquidityscanner/MainActivity.kt
@@ -1,1 +1,25 @@
+package com.alexsentex.liquidityscanner
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.alexsentex.liquidityscanner.ui.MainScreen
+import com.alexsentex.liquidityscanner.ui.MainViewModel
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MaterialTheme {
+                Surface {
+                    val vm: MainViewModel = viewModel()
+                    MainScreen(vm)
+                }
+            }
+        }
+    }
+}
