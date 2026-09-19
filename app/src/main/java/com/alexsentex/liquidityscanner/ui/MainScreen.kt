aab25045d867955cb5da15a5444a309bf74eb98a
Oleksandr <107505831+AlexSentex@users.noreply.github.com>
2026-09-19 16:31:20 +0300

    Add MainScreen composable to display title

---
+++ app/src/main/java/com/alexsentex/liquidityscanner/ui/MainScreen.kt
@@ -0,0 +1,9 @@
+package com.alexsentex.liquidityscanner.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainScreen(viewModel: MainViewModel) {
+    Text(text = "Liquidity Scanner")
+}
