69da660812b60bdca4243e95862b7bf5055db220
Oleksandr <107505831+AlexSentex@users.noreply.github.com>
2026-09-20 20:39:12 +0300

    Add new properties to LiquidityZone data class

--- app/src/main/java/com/alexsentex/liquidityscanner/model/LiquidityZone.kt
+++ app/src/main/java/com/alexsentex/liquidityscanner/model/LiquidityZone.kt
@@ -7,12 +7,34 @@ enum class LiquidityZoneType {
 
 data class LiquidityZone(
     val type: LiquidityZoneType,
+
     val lowerPrice: Double,
     val upperPrice: Double,
     val centerPrice: Double,
+
     val totalQuantity: Double,
     val totalUsdt: Double,
+
     val levelCount: Int,
+
     val strength: Double,
-    val distancePercent: Double
-)
+
+    val distancePercent: Double,
+
+    val firstSeenMillis: Long,
+    val lastSeenMillis: Long,
+
+    val minQuantity: Double,
+    val maxQuantity: Double,
+
+    val observationCount: Int,
+
+    val stabilityPercent: Double
+) {
+
+    val lifetimeMillis: Long
+        get() = lastSeenMillis - firstSeenMillis
+
+    val lifetimeMinutes: Double
+        get() = lifetimeMillis / 60_000.0
+}
